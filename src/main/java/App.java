2d00499dc83682fe9b5323e849254963e0044770
term10 <spdojeisfd@mail.ru>
2024-02-27 12:39:04 +0300

    try catch

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,16 +3,28 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите выражение ");
+        System.out.println("Введите выражение: ");
         String input = scanner.nextLine();
         String[] parts = input.split(" ");
         if(parts.length != 3) {
             System.out.println("Ошибка: Введите выражение в формате 'число оператор число'");
             return;
         }
-        int num1 = Integer.parseInt(parts[0]);
+        int num1;
+        try {
+            num1 = Integer.parseInt(parts[0]);
+        }catch (NumberFormatException e) {
+            System.out.println("Не удалось получить число из: ");
+            return;
+            }
+        int num2;
+        try {
+            num2 = Integer.parseInt(parts[2]);
+        }catch (NumberFormatException e) {
+            System.out.println("Не удалось получить число из: ");
+            return;
+        }
         char operator = parts[1].charAt(0);
-        int num2 = Integer.parseInt(parts[2]);
         int result = 0;
 
         switch(operator) {
